270cd3b3d5d5eee4d6a285ce0eea9d2468916ecd
Tigran18 <tigranda1809@HP-Laptop1809.>
2025-03-09 01:31:35 +0400

    Lazy summator-row class moved to add class private section

--- lazy_summator/lazy_summator.cpp
+++ lazy_summator/lazy_summator.cpp
@@ -8,24 +8,24 @@ private:
     size_t column;
     std::vector<std::vector<T>> vec1;
     std::vector<std::vector<T>> vec2;
+    class Row {
+        private:
+            const std::vector<T>& vec1_row;
+            const std::vector<T>& vec2_row;
+    
+        public:
+            Row(const std::vector<T>& vec1_row, const std::vector<T>& vec2_row) 
+                : vec1_row(vec1_row), vec2_row(vec2_row) {}
+    
+            T operator[](size_t j) const {
+                return vec1_row[j] + vec2_row[j];
+            }
+        };
 
 public:
     Add(const std::vector<std::vector<T>>& vec1, const std::vector<std::vector<T>>& vec2) 
         : vec1(vec1), vec2(vec2), row(vec1.size()), column(vec1[0].size()) {}
 
-    class Row {
-    private:
-        const std::vector<T>& vec1_row;
-        const std::vector<T>& vec2_row;
-
-    public:
-        Row(const std::vector<T>& vec1_row, const std::vector<T>& vec2_row) 
-            : vec1_row(vec1_row), vec2_row(vec2_row) {}
-
-        T operator[](size_t j) const {
-            return vec1_row[j] + vec2_row[j];
-        }
-    };
 
     Row operator[](size_t i) const {
         return Row(vec1[i], vec2[i]);
