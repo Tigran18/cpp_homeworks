bc185dc7258ec2b187a80fee1b5ace5f1ce9ff52
Tigran18 <tigrandatyan18@gmail.com>
2025-03-29 14:24:27 +0400

    Changed allocator method in order not to create an instance.

--- allocator/allocator.h
+++ allocator/allocator.h
@@ -7,12 +7,12 @@ template <typename T>
 struct SimpleAllocator {
     using value_type = T;
 
-    SimpleAllocator() noexcept {}
+    SimpleAllocator()=default;
 
     template <typename U>
     SimpleAllocator(const SimpleAllocator<U>&) noexcept {}
 
-    T* allocate(std::size_t n) {
+    [[nodiscard]] static T* allocate(std::size_t n) {
         if (n == 0) {
             return nullptr;
         }
@@ -23,7 +23,7 @@ struct SimpleAllocator {
         return ptr;
     }
 
-    void deallocate(T* ptr, std::size_t n) noexcept {
+    static void deallocate(T* ptr, std::size_t n) noexcept {
         ::operator delete(ptr);
     }
 };
--- allocator/test.cpp
+++ allocator/test.cpp
@@ -1,16 +1,37 @@
-#include <iostream>
 #include "allocator.h"
+#include <vector>
+#include <iostream>
 
 int main() {
-    SimpleAllocator<int> alloc;
+    try {
+        std::vector<int, SimpleAllocator<int>> vec;
+        vec.push_back(10);
+        vec.push_back(20);
+        vec.push_back(30);
 
-    int* p = alloc.allocate(10);
-    for(int i=0; i<10; i++){
-        *(p+i)=i*i;
+        std::cout << "Vector elements: ";
+        for (int val : vec) {
+            std::cout << val << " ";
+        }
+        std::cout << "\n";
+
+        int* ptr = SimpleAllocator<int>::allocate(5);
+
+        for (int i = 0; i < 5; ++i) {
+            ptr[i] = i * 10;
+        }
+
+        std::cout << "Allocated values: ";
+        for (int i = 0; i < 5; ++i) {
+            std::cout << ptr[i] << " ";
+        }
+        std::cout << "\n";
+
+        SimpleAllocator<int>::deallocate(ptr, 5);
+        std::cout << "Memory deallocated successfully.\n";
+
+    } catch (const std::bad_alloc& e) {
+        std::cerr << "Allocation failed: " << e.what() << "\n";
     }
-    for(int i=0; i<10; i++){
-        std::cout<<*(p+i)<<std::endl;
-    }
-    alloc.deallocate(p, 5);
     return 0;
 }
