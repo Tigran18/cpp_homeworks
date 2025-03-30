78616dfaa611df68cfd2dc2d6fe81e602340c3c1
Tigran18 <tigrandavtyan18@gmail.com>
2025-03-30 12:21:20 +0400

    Merged.

--- memory_pool/memory_poll.hpp
+++ memory_pool/memory_poll.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include "allocator.hpp"
-<<<<<<< HEAD
 #include <vector>
 #include <cstdlib>
 
@@ -58,42 +57,3 @@ char* MemoryPool::pool = nullptr;
 std::size_t MemoryPool::blockSize = 0;
 std::size_t MemoryPool::blockCount = 0;
 std::vector<char*> MemoryPool::freeBlocks;
-=======
-
-class MemoryPool {
-    private:
-        char* pool;
-        std::size_t blockSize;
-        std::size_t blockCount;
-        std::vector<char*> freeBlocks;
-        
-    public:
-        MemoryPool(std::size_t blockSize, std::size_t blockCount)
-            : blockSize(blockSize), blockCount(blockCount) {
-            pool = SimpleAllocator<char>::allocate(blockSize * blockCount);
-            
-            for (std::size_t i = 0; i < blockCount; ++i) {
-                freeBlocks.push_back(pool + i * blockSize);
-            }
-        }
-    
-        ~MemoryPool() {
-            SimpleAllocator<char>::deallocate(pool, blockSize * blockCount);
-            pool=nullptr;
-        }
-    
-        void* allocate() {
-            if (freeBlocks.empty()) {
-                throw std::bad_alloc();
-            }
-            void* block = freeBlocks.back();
-            freeBlocks.pop_back();
-            return block;
-        }
-    
-        void deallocate(void* block) {
-            freeBlocks.push_back(static_cast<char*>(block));
-        }
-    };
-    
->>>>>>> refs/remotes/origin/main
--- memory_pool/test.cpp
+++ memory_pool/test.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-<<<<<<< HEAD
 #include "memory_poll.hpp"
 
 int main() {
@@ -15,21 +14,5 @@ int main() {
 
     MemoryPool::cleanup();
 
-=======
-#include <vector>
-#include "memory_poll.hpp"
-
-int main() {
-    MemoryPool pool(32, 10);
-
-    void* ptr1 = pool.allocate();
-    void* ptr2 = pool.allocate();
-    std::cout << "Allocated: " << ptr1 << " and " << ptr2 << std::endl;
-
-    pool.deallocate(ptr1);
-    pool.deallocate(ptr2);
-    std::cout << "Deallocated both pointers." << std::endl;
-    
->>>>>>> refs/remotes/origin/main
     return 0;
 }
