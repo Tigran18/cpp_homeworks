8b70d2d39924ae8a7d6463e17593f332daa2ffd1
Tigran18 <tigrandavtyan18@gmail.com>
2025-05-11 12:44:30 +0400

    grep created. Started implementing.

---
+++ grep/grep.cpp
@@ -0,0 +1,2 @@
+#include "grep.hpp"
+
---
+++ grep/test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "grep.hpp"
+#include <string>
+
+int main() {
+    std::string command;
+    while (true) {
+        std::getline(std::cin, command);
+        if (command.empty()) continue;
+        std::string first_word;
+        size_t i = 0;
+        while (i < command.length() && command[i] != ' ') {
+            first_word += command[i++];
+        }
+        if (first_word != "grep") {
+            std::cout << "Command isn't grep\n";
+        } else {
+            std::cout << "Command is grep\n";
+        }
+    }
+
+    return 0;
+}
